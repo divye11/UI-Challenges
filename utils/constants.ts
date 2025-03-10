0a1bf2a55f97faf01db30d81a8d7939b9195dda9
Skiball <divye11@gmail.com>
2025-03-10 16:53:33 +0530

    feat: comments UI added

---
+++ utils/constants.ts
@@ -0,0 +1,14 @@
+export const projectList: Project[] = [
+   {
+      id: '1',
+      name: 'Comments UI',
+      url: '/comments',
+      description: 'Comments UI where people can react and add comments inside a comment'
+   }, 
+   {
+      id: '2',
+      name: 'Feature Flag',
+      url: '/ff',
+      description: 'Feature flag implementation'
+   }
+]
